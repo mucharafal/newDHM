cccdc78279fac86716aca31191624bdf25f8486e
Rafał Mucha <6rafel6@gmail.com>
2019-03-26 11:47:38 +0100

    Modified approach

--- DistributedHashMap.java
+++ DistributedHashMap.java
@@ -11,149 +11,105 @@ import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.List;
 
-public class DistributedHashMap<K, V> {
-    private static String groupAddressString = "230.100.200.9";
-    private static String groupSynchronizationAddressString = "230.100.200.10";
-    private static String channelName = "seleythen";
-    private static String channelSynchronization = "synchro";
-    private InetAddress groupAddress;
-    private InetAddress groupSynchronizationAddress;
+public class DistributedHashMap<K, V> extends ReceiverAdapter{
 
-    private class Server extends ReceiverAdapter {
-        private HashMap<K, V> map = null;
-        private JChannel channel;
-        private JChannel synchronizationChannel;
-        Server() throws Exception{
-            channel = initializeChannel(groupAddress);
-            channel.setReceiver(this);
-            channel.connect(channelName);
-
-            synchronizationChannel = initializeChannel(groupSynchronizationAddress);
-            synchronizationChannel.connect(channelSynchronization);
-
-            System.out.println("Before getState");
-            if(synchronizationChannel.getView().size() != 1){
-                synchronizationChannel.getState(null, 12000, true);
-            }
-            System.out.println("After getState");
-            if(map == null){
-                map = new HashMap<>();
-            }
-        }
-
-        V getElement(K key){
-            return map.get(key);
-        }
-
-        boolean containsKey(K key){
-            return map.containsKey(key);
-        }
-
-        @Override
-        public void getState(OutputStream output) throws Exception {
-            System.out.println("state get, map: " + map.size());
-
-            output.write(Util.objectToByteBuffer(map));
-        }
-
-        @Override
-        public void setState(InputStream input) throws Exception {
-            System.out.println("state set, map: " + map.size());
-            map = (HashMap<K, V>)Util.objectFromStream(new DataInputStream(input));
-        }
-
-        public void receive(Message msg){
-            processMessage(msg);
-        }
-
-        public void receive(MessageBatch batch){
-            for(Message msg: batch){
-                processMessage(msg);
-            }
-        }
-
-        private void processMessage(Message msg){
-            System.out.println("Received message");
-            MyMessage<K, V> message;
-            try {
-                message = (MyMessage<K, V>)msg.getObject();
-                switch (message.type){
-                    case ADD:
-                        map.put(message.key, message.value);
-                        break;
-                    case REMOVE:
-                        map.remove(message.key);
-                        break;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void viewAccepted(View view){
-            if(view instanceof MergeView){
-                MergeView tmp = (MergeView)view;
-                List<View> subgroups = tmp.getSubgroups();
-                Address myAddress = synchronizationChannel.getAddress();
-                if(!subgroups.get(0).containsMember(myAddress)){
-                    synchronizationChannel.getState();
-                }
-            }
-        }
-    }
-    private class Client{
-        JChannel channel;
-        Client() throws Exception {
-            channel = initializeChannel(groupAddress);
-            channel.connect(channelName);
-        }
-
-        void add(K key, V value) throws Exception {
-            MyMessage message = new MyMessage(RequestType.ADD, key, value);
-            channel.send(new Message(null, message));
-        }
-
-        void remove(K key) throws Exception {
-            MyMessage message = new MyMessage(RequestType.REMOVE, key, null);
-            channel.send(new Message(null, message));
-        }
-    }
-
-    private Client client;
-    private Server server;
-
-    public DistributedHashMap() throws Exception{
-        setupProperties();
-        client = new Client();
-        server = new Server();
-    }
-    public void addElement(K key, V value) throws Exception{
-        client.add(key, value);
-    }
+    private static String stringAddress = "230.100.200.11";
+    private static String channelName = "synchro";
+    
+    private HashMap<K, V> map = new HashMap<>();
+    private JChannel channel;
 
     public V getElement(K key){
-        return server.getElement(key);
+        return map.get(key);
     }
 
     public boolean containsKey(K key){
-        return server.containsKey(key);
+        return map.containsKey(key);
     }
 
-    public void remove(K key) throws Exception{
-        client.remove(key);
+    public void addElement(K key, V value) throws Exception {
+        MyMessage message = new MyMessage(RequestType.ADD, key, value);
+        channel.send(new Message(null, message));
     }
 
-    private JChannel initializeChannel(InetAddress address) throws Exception{
+    public void remove(K key) throws Exception {
+        MyMessage message = new MyMessage(RequestType.REMOVE, key, null);
+        channel.send(new Message(null, message));
+    }
+
+    @Override
+    public void getState(OutputStream output) throws Exception {
+        System.out.println("Get state ; insides");
+        ObjectOutputStream outputStream = new ObjectOutputStream(output);
+        outputStream.writeObject(map);
+        System.out.println("state get, map: " + map.size());
+    }
+
+    @Override
+    public void setState(InputStream input) throws Exception {
+        map = (HashMap<K, V>)new ObjectInputStream(input).readObject();
+    }
+
+    public void receive(Message msg){
+        processMessage(msg);
+    }
+
+    public void receive(MessageBatch batch){
+        for(Message msg: batch){
+            processMessage(msg);
+        }
+        try {
+            channel.getState(null, 12000, true);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void processMessage(Message msg){
+        System.out.println("Received message");
+        MyMessage<K, V> message;
+        try {
+            message = (MyMessage<K, V>)msg.getObject();
+            switch (message.type){
+                case ADD:
+                    map.put(message.key, message.value);
+                    break;
+                case REMOVE:
+                    map.remove(message.key);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void viewAccepted(View view){
+        if(view instanceof MergeView){
+            MergeView tmp = (MergeView)view;
+            List<View> subgroups = tmp.getSubgroups();
+            Address myAddress = channel.getAddress();
+            if(!subgroups.get(0).containsMember(myAddress)){
+                channel.getState();
+            }
+        }
+    }
+
+    public DistributedHashMap() throws Exception{
+        setupProperties();
+        initializeChannel();
+    }
+
+    private void initializeChannel() throws Exception{
         UDP udp = new UDP();
-        udp.setValue("mcast_group_addr", address);
+        udp.setValue("mcast_group_addr", InetAddress.getByName(stringAddress));
         ProtocolStack stack = new ProtocolStack();
-        JChannel channel = new JChannel(false);
+        channel = new JChannel(false);
         channel.setProtocolStack(stack);
         stack.addProtocol(udp)
                 .addProtocol(new PING())
                 .addProtocol(new MERGE3())
                 .addProtocol(new FD_SOCK())
-                .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval",3000 ))
+                .addProtocol(new FD_ALL().setValue("timeout",12000).setValue("interval",3000 ))
                 .addProtocol(new VERIFY_SUSPECT())
                 .addProtocol(new BARRIER())
                 .addProtocol(new NAKACK2())
@@ -163,15 +119,19 @@ public class DistributedHashMap<K, V> {
                 .addProtocol(new UFC())
                 .addProtocol(new MFC())
                 .addProtocol(new FRAG2())
-                .addProtocol(new FLUSH())
-                .addProtocol(new STATE());
+                .addProtocol(new STATE())
+                .addProtocol(new SEQUENCER())
+                .addProtocol(new FLUSH());
         stack.init();
-        return channel;
+
+        channel.connect(channelName);
+
+        channel.setReceiver(this);
+
+        channel.getState(null, 12000, true);
     }
 
     private void setupProperties() throws Exception{
         System.setProperty("java.net.preferIPv4Stack","true");
-        groupAddress = InetAddress.getByName(groupAddressString);
-        groupSynchronizationAddress = InetAddress.getByName(groupSynchronizationAddressString);
     }
 }
